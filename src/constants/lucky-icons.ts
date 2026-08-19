299e367b0c09d116753272943b024b207ca136c3
rose_lee <3484832080079191-rose_lee@noreply.coze.cn>
2026-08-19 21:30:41 +0800

    feat: 幸运指数使用 luckystar 压缩素材并随机展示
    Coze-Commit-Type: user
    Coze-User-ID: 3484832080079191
    Coze-Conversation-ID: 10974642

---
+++ src/constants/lucky-icons.ts
@@ -0,0 +1,11 @@
+/**
+ * 幸运指数 icon 素材（已从 TOS 压缩后生成）
+ * 每次展示时随机选取
+ */
+export const LUCKY_STAR_ICONS = [
+  'https://coze-coding-project.tos.coze.site/coze_storage_7665650076865331200/luckystar/IP_1_compressed_f0d6e24a.png?sign=1789738121-9b55800925-0-dcef8ef8d00c0c0da957a50921b1f4ab5fe1748f16226b2d306cdb7776c7f6e6',
+  'https://coze-coding-project.tos.coze.site/coze_storage_7665650076865331200/luckystar/IP_2_compressed_a57a6f9c.png?sign=1789738122-1fb79fc72f-0-00165c12c7e9c9797fbbaddf912b622dbbda87e9536f1d4f9d5cdde4ce2a3146',
+  'https://coze-coding-project.tos.coze.site/coze_storage_7665650076865331200/luckystar/IP_3_compressed_87ca7563.png?sign=1789738123-5270005b63-0-54982d9548cac7c719645cdf1d42f9fce1f61253674d9d79eb98b4c76e94f79a',
+  'https://coze-coding-project.tos.coze.site/coze_storage_7665650076865331200/luckystar/IP_4_compressed_89c2e4f5.png?sign=1789738123-e4c79d7770-0-93b915f75a3efe7ac752b68abbaa87e9f1e5fa206eeb132e024378427b63bf85',
+  'https://coze-coding-project.tos.coze.site/coze_storage_7665650076865331200/luckystar/IP_compressed_6a323948.png?sign=1789738124-be337a6f4c-0-df2c92a2daa5bee206bb244de7c279b6a92c4cd60fb40542370ad498d0a7914b',
+]
